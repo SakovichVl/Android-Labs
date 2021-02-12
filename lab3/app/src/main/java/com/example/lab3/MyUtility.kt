4d66ac900eeed35157b9a21df62a5bea83f21e60
Sakovich Vlad <vladkvadrat3000@gmail.com>
2021-02-12 14:42:15 +0300

    add lab3

---
+++ lab3/app/src/main/java/com/example/lab3/MyUtility.kt
@@ -0,0 +1,19 @@
+package com.example.lab3
+
+import java.math.BigInteger
+import java.security.MessageDigest
+
+class MyUtility {
+    companion object{
+        fun algo_MD5(mail: String): String? {
+            var result: String? = null
+            val messageDigest = MessageDigest.getInstance("MD5")
+            messageDigest.reset()
+            messageDigest.update(mail.toByteArray())
+            val bigInt = BigInteger(1, messageDigest.digest())
+            result = bigInt.toString(16)
+            return result
+        }
+
+    }
+}
